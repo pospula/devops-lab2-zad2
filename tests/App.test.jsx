af42c09a3aff4a8fbd45d9374015a40128b01a9d
pospula <pospula@student.wszib.edu.pl>
2025-12-14 08:59:06 +0100

    Fix App.test.jsx

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -4,7 +4,7 @@ import App from '../src/App'
 describe('App component', () => {
   test('renders with count 0', () => {
     render(<App />)
-    expect(true).toBe(false)
+    expect(screen.getByText(/count is 0/i)).toBeInTheDocument()
   })
 
   test('increments count when button clicked', () => {
